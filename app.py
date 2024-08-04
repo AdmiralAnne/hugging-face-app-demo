f9ee11e687b2c71113e4b50f13f2e2d86106f066
Marjiba Jamir <marjiba@users.noreply.huggingface.co>
2024-08-04 12:23:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from transformers import pipeline
 
 st.title('Hugging face NLP Test')
+
 st.info('coming soon')
 
 model=pipeline('sentiment-analysis')
+
+result=model("this is a test to see how well the model works")
+result
